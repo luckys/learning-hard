bd87355f4e97a8f13a77a3534ffe010344b5ab03
Luis Ramirez Calle <luis.ramirezcalle@outlook.com>
2025-11-13 07:27:16 +0000

    feat: Initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 5,
+  },
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+  ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [{
+      code: 'en',
+      name: 'English',
+    }, {
+      code: 'fr',
+      name: 'Français',
+    }],
+  },
+})
